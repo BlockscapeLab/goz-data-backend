b313e35e2c2762d10ec1cf7336f254e88c357738
Douglas Sondheim <douglas.sondheim@mway.io>
2020-05-15 19:19:34 +0200

    use dataprovider instead of moc

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"log"
+	"os"
+
+	"github.com/BlockscapeLab/goz-data-backend/chain"
 	"github.com/BlockscapeLab/goz-data-backend/data"
 	"github.com/BlockscapeLab/goz-data-backend/rest"
 )
 
 func main() {
-	//dp := &data.DataProvider{}
-	dp := data.MockProvider{}
+	lcd, err := chain.CreateLCDConnector("127.0.0.1", 1317)
+	if err != nil {
+		log.Println("Error while setting up connection to lcd:", err)
+		os.Exit(1)
+	}
+
+	dp, err := data.NewDataProvider("2020-05-14T00:00:00Z", "2020-05-28T00:00:00Z", lcd)
+	if err != nil {
+		log.Println("Couldn't set up DataProvider:", err)
+		os.Exit(1)
+	}
+	//dp := data.MockProvider{}
 	rest.StartRestServer("0.0.0.0", 8080, dp)
 }
