f731a29e4d9c7209b5224ae074fe1d477fd745ee
Douglas Sondheim <douglas.sondheim@mway.io>
2020-05-15 11:04:45 +0200

    start server as mock

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"github.com/BlockscapeLab/goz-data-backend/data"
+	"github.com/BlockscapeLab/goz-data-backend/rest"
+)
 
+func main() {
+	//dp := &data.DataProvider{}
+	dp := data.MockProvider{}
+	rest.StartRestServer("0.0.0.0", 8080, dp)
 }
